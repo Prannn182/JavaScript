06a37c23986ed5bbe064795d7cedeec682f68d8d
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-01-29 21:44:25 +0530

    Add files via upload

---
+++ Tutorial7/index.js
@@ -0,0 +1,27 @@
+let arr=[1,2,3,4,5,6];
+console.log(arr);
+console.log(arr.length);
+arr[0]=5667;
+console.log(arr);
+// console.log(arr, typeof arr);
+// console.log(arr.toString());
+// console.log(arr.join(" and "));
+// arr.pop(); // will pop last element 
+// console.log(arr);
+// arr.push("new item"); //will push new item at the end
+// console.log(arr);
+// arr.shift(); //will remove first element
+// console.log(arr);
+// arr.unshift("Prasad"); //will add element at the beginning
+// console.log(arr);
+// delete arr[5]; //will delete element at index 5
+
+// let arr2=[10,20,30,40,50];
+// console.log(arr.concat(arr2)); //will concatenate arr and arr2
+// console.log(arr.sort());
+// console.log(arr2.splice(0,2))
+// console.log(arr2);
+// arr2.splice(0,2,100,200);
+// console.log(arr2);
+arr.slice(1,2);
+console.log(arr);
---
+++ Tutorial7/loops.js
@@ -0,0 +1,7 @@
+let arr=[1,52,3,44,5,6];
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+   
+}
+console.log(arr.sort());
