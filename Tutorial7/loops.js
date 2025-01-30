837bdcfed44665b6f9634ce8a06b4d7b768f394b
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-01-30 22:56:47 +0530

    Update loops.js

--- Tutorial7/loops.js
+++ Tutorial7/loops.js
@@ -1,7 +1,27 @@
 let arr=[1,52,3,44,5,6];
-for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    console.log(element);
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);
    
+
+arr.forEach((value,index,arr) => {
+    console.log(value,index);
+});
+// console.log(arr.sort());
+let a={
+    1:1,
+    2:2,
+    3:3
 }
-console.log(arr.sort());
+//for in loop
+for (const key in a) {
+    if (Object.prototype.hasOwnProperty.call(a, key)) {
+        const element = a[key];
+        console.log(key,element);
+        
+    }
+}
+///for of loop
+for (const element of arr) {
+    console.log(element);
+}
