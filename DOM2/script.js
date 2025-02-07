b06b2fb0c9ecb5dc12a6f8331884ceca2e0a5fe2
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-02-07 19:59:06 +0530

    Add files via upload

---
+++ DOM2/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+document.body.firstElementChild;
+
+document.body.firstElementChild.firstChild;
+
+document.body.firstElementChild.childNodes;
+NodeList(10)[
+  (text, div.box, text, div.box, text, div.box, text, div.box, text, div.box)
+];
+document.body.firstElementChild.children
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.children[1]
+document.body.children[1].rows
