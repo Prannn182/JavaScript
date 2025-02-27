8f3dc1f13213841cc5ee86c5e52d2d9a16f42d25
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-02-27 22:35:59 +0530

    Update script.js

--- Hacking Simulator/script.js
+++ Hacking Simulator/script.js
@@ -1,49 +1,45 @@
 alert("you can be get hacked");
-function getRandomnum() {
-  return Math.floor(Math.random() * 7000);
+const addItem = async (item) => {
+  await getRandomnum();
+ let div= document.createElement("div");
+  div.innerHTML = item;
+  document.body.append(div);
+};
+const getRandomnum=()=> {
+  return new Promise((resolve) => {
+    let timeout = Math.floor(Math.random() * 7000);
+    setTimeout(() => {
+      resolve(timeout);
+    }, timeout);
+  });
 }
 
-async function hack() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Initializing hacking...");
-    }, getRandomnum());
-  });
+
+async function startHacking(){
+    let text = [
+        "Hacking Initialized",
+        "Reading your files",
+        "Hacking your webcam",
+        "Hacking your microphone",
+        "Hacking your location",
+        "Hacking your bank",
+        "Successfully hacked",
+        "You are hacked",
+      ];
+      setInterval(() => {
+        let last=document.body.lastElementChild;
+        if(last.innerHTML.endsWith("...")){
+            last.innerHTML=last.innerHTML.slice(0,last.innerHTML.length-3);
+        }else{
+            last.innerHTML=last.innerHTML+"."
+        }
+      },500)
+
+    for (let items of text) 
+        {
+          await addItem(items);
+       } 
 }
-async function hack1() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("hacking started...");
-    }, getRandomnum());
-  });
-}
-async function hack2() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Fetching data...");
-    }, getRandomnum());
-  });
-}
-async function hack3() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Hacking completed...");
-    }, getRandomnum());
-  });
-}
-// function display(message) {
-//    let p= document.createElement("p")
-//     p.textContent=message
-//     document.body.appendChild(p)
-// }
-async function MediaDeviceInfo() {
-  let a= await hack();
-    console.log(a);
-    let b = await hack1();
-    console.log(b);
-    let c = await hack2();
-    console.log(c);
-    let d = await hack3();
-    console.log(d);
-}
-MediaDeviceInfo();
+startHacking()
+
+
