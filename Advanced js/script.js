c40950f6364c9f1012c015e14d66169db6634ace
Pranali chobe <89305305+Prannn182@users.noreply.github.com>
2025-02-27 23:51:19 +0530

    Update script.js

--- Advanced js/script.js
+++ Advanced js/script.js
@@ -6,8 +6,13 @@ async function sleep() {
     }, 1000);
   });
 }
-
+//Destructuring
 (async function main() {
   let a = await sleep();
   console.log(a);
+  let[x,y,...j]=[1,2,6,8,9,77,15,46,32];
+    console.log(x,y,j);
 })();
+
+
+
